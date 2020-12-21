dd2a20212d97dd207f2ffbfc2864f578641195a6
mshuber1981 <michael.huber1981@outlook.com>
2020-12-21 15:41:46 -0600

    Cloning/updating project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import ContactForm from "./assets/containers/ContactForm";
 
+// API Gateway Contact Form endpoint
+const apiUrl = "Your API endpoint URL";
+
 const App = () => {
   return (
     <main>
-      <ContactForm />
+      <ContactForm apiUrl={apiUrl} />
     </main>
   );
 };
