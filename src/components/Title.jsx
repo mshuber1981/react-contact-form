1488d477afeff5f76a3ef4aa7a72de9865b2cf16
Michael Huber <michael.huber1981@outlook.com>
2022-08-06 11:20:51 -0500

    Updating underline styles

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -8,6 +8,8 @@ export const Title = styled.div`
   .underline {
     height: 0.25rem;
     width: 75%;
+    min-width: 3rem;
+    border-radius: 0.25rem;
     background: var(--clr-primary-5);
     margin: 0 auto 1.5rem auto;
     background: ${({ theme }) =>
