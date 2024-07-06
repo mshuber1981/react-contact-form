397d6c5b546d5813f25e12c8e13e3fa4cb39a1c7
mshuber1981 <michael.huber1981@outlook.com>
2024-07-06 07:32:05 -0500

    Removing config.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import ghPages from "./scripts/gh-pages";
-import Config from "./config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,7 +10,7 @@ export default defineConfig(({ mode }) => {
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-    base: `/${Config.repoName}/`,
+    base: "react-contact-form",
     plugins: [react(), ghPages()],
   };
 });
